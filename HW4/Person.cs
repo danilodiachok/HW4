af7c129c2b37627ce1fea0fad609348202113600
danilo_cj <danilo_cj@DESKTOP-BT8DAVV>
2023-10-05 16:30:19 +0300

    Add project files.

---
+++ HW4/Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birthYear; 
+
+        
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+        }
+
+        
+        public Person()
+        {
+            name = "";
+            birthYear = DateTime.MinValue;
+        }
+
+        
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        
+        public int Age()
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthYear.Year;
+
+            
+            if (currentDate < birthYear.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        
+        public void Input()
+        {
+            Console.Write("Введіть ім'я: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введіть рік народження (yyyy): ");
+            if (DateTime.TryParseExact(Console.ReadLine(), "yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime parsedYear))
+            {
+                birthYear = parsedYear;
+            }
+            else
+            {
+                Console.WriteLine("Невірний формат введення року народження.");
+            }
+        }
+
+        
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        
+        public override string ToString()
+        {
+            return $"Ім'я: {name}, Рік народження: {birthYear:yyyy}, Вік: {Age()}";
+        }
+
+        
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+       
+        public static bool operator ==(Person person1, Person person2)
+        {
+            if (ReferenceEquals(person1, person2))
+                return true;
+
+            if (person1 is null || person2 is null)
+                return false;
+
+            return person1.name == person2.name;
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !(person1 == person2);
+        }
+
+        
+    }
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person[] people = new Person[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Введіть інформацію для особи {i + 1}:");
+                string name = Console.ReadLine();
+                Console.Write("Введіть рік народження (yyyy): ");
+                if (DateTime.TryParseExact(Console.ReadLine(), "yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime birthYear))
+                {
+                    people[i] = new Person(name, birthYear);
+                }
+                else
+                {
+                    Console.WriteLine("Невірний формат введення року народження. Особа не створена.");
+                }
+            }
+
+            
+            Console.WriteLine("\nІнформація про створених осіб:");
+            foreach (Person person in people)
+            {
+                if (person != null)
+                {
+                    person.Output();
+                }
+            }
+            
+            Console.WriteLine("\nРозрахунок і виведення імен та віку кожної особи:");
+            foreach (Person person in people)
+            {
+                if (person != null)
+                {
+                    Console.WriteLine($"Ім'я: {person.Name}, Вік: {person.Age()}");
+                }
+            }
+
+           
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i] != null && people[i].Age() < 16)
+                {
+                    people[i].ChangeName("Very Young");
+                }
+            }
+
+            
+            Console.WriteLine("\nІнформація про осіб після зміни імен:");
+            foreach (Person person in people)
+            {
+                if (person != null)
+                {
+                    person.Output();
+                }
+            }
+            
+            Console.WriteLine("\nІнформація про всіх осіб:");
+            foreach (Person person in people)
+            {
+                if (person != null)
+                {
+                    person.Output();
+                }
+            }
+
+
+            Console.WriteLine("\nОсоби з однаковими іменами:");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (people[i] != null && people[j] != null && people[i] == people[j])
+                    {
+                        Console.WriteLine($"Особа {i + 1} та особа {j + 1} мають однакове ім'я: {people[i].Name}");
+                    }
+                }
+            }
+
+            
+    
+        }
+    }
+}
